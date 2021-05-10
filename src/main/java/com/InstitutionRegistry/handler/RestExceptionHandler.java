9451228921092e27ec4b5f6a0f4bb6f148fad0ca
cariasdaniel <d.carias@ymail.com>
2021-05-10 17:13:56 -0300

    Added ExceptionHandler

--- src/main/java/com/InstitutionRegistry/handler/RestExceptionHandler.java
+++ src/main/java/com/InstitutionRegistry/handler/RestExceptionHandler.java
@@ -1,9 +1,21 @@
 package com.InstitutionRegistry.handler;
 
+import org.hibernate.PropertyValueException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(value = {PropertyValueException.class})
+    protected ModelAndView handleInvalidInput(RuntimeException ex, WebRequest request){
+        ModelAndView mv = new ModelAndView("error", HttpStatus.BAD_REQUEST);
+        return mv;
+    }
 }
